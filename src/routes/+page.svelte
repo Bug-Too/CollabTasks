2f7d6bdb45cb6f4f390f751b6e2475fb1a56dc4b
pooh <purichseenuallae@gmail.com>
2023-03-07 00:33:03 +0700

    Initial commit and add login page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="primary-background h-screen self-center text-center pt-24">
+	<h1 class="text-5xl font-bold ">CollabTask 📝</h1>
+	<div class="flex items-center justify-center h-5/6 ">
+		<a href="./home" class="">
+			<button
+				class="text-black bg-white px-4 py-4 rounded-2xl flex items-center text-xl justify-center place-self-center"
+			>
+				<div class="pr-2">
+					<Icon icon="ri:google-fill" class="text-3xl" />
+				</div>
+				 Sign in with Google
+			</button>
+		</a>
+	</div>
+</div>
+
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,6 @@
+<div class="primary-background h-screen self-center text-center pt-24">
+    <h1 class="text-3xl font-bold"> <span>Day</span> Month Year </h1>
+    <div>
+        
+    </div>
+</div>
