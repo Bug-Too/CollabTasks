4d224640539ef57972f4af5908d34a1636764508
pooh <purichseenuallae@gmail.com>
2023-03-13 21:43:39 +0700

    Add circle button

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -7,6 +7,42 @@ class TodoPage extends StatefulWidget {
   State<StatefulWidget> createState() => _TodoPageState();
 }
 
+class CircleIconButton extends StatelessWidget {
+  final IconData iconData;
+  final double size;
+  final Color color;
+  final Color iconColor;
+  final VoidCallback onPressed;
+
+  CircleIconButton({
+    required this.iconData,
+    required this.size,
+    required this.color,
+    required this.iconColor,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          shape: CircleBorder(),
+        ),
+        child: Icon(
+          iconData,
+          color: iconColor,
+          size: size * 0.6,
+        ),
+      ),
+    );
+  }
+}
+
 class _TodoPageState extends State<TodoPage> {
   var date = DateTime.now();
 
@@ -23,13 +59,15 @@ class _TodoPageState extends State<TodoPage> {
     if (picked != null && picked != selectedDate) {
       selectedDate = picked;
     }
-
+    setState(() {
+      date = selectedDate!;
+    });
     return selectedDate;
   }
 
   @override
   Widget build(BuildContext context) {
-    String getCurrentDate() {
+    List getCurrentDate() {
       const months = [
         'January',
         'February',
@@ -44,17 +82,20 @@ class _TodoPageState extends State<TodoPage> {
         'November',
         'December'
       ];
-      return '${date.day.toString()} ${months[date.month]} ${date.year.toString()}';
+      return [
+        date.day.toString(),
+        ' ${months[date.month]} ${date.year.toString()}'
+      ];
     }
 
-    String nextDate() {
+    List nextDate() {
       setState(() {
         date = date.add(const Duration(days: 1));
       });
       return getCurrentDate();
     }
 
-    String previousDate() {
+    List previousDate() {
       setState(() {
         date = date.subtract(const Duration(days: 1));
       });
@@ -65,8 +106,7 @@ class _TodoPageState extends State<TodoPage> {
     Widget dateBar() {
       return GestureDetector(
         onTap: () async {
-          DateTime? selectedDate = await _selectDate(context);
-          print(selectedDate);
+          await _selectDate(context);
         },
         child: Container(
             margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
@@ -84,7 +124,22 @@ class _TodoPageState extends State<TodoPage> {
                     previousDate();
                   },
                 ),
-                Text(getCurrentDate(), style: const TextStyle(fontSize: 20)),
+                RichText(
+                  text: TextSpan(
+                    text: '',
+                    style: const TextStyle(color: Colors.black, fontSize: 24),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: getCurrentDate()[0],
+                          style: const TextStyle(
+                            color: Colors.red,
+                          )),
+                      TextSpan(
+                        text: getCurrentDate()[1],
+                      )
+                    ],
+                  ),
+                ),
                 IconButton(
                   icon: const Icon(Icons.arrow_forward_ios),
                   onPressed: () {
@@ -102,7 +157,37 @@ class _TodoPageState extends State<TodoPage> {
       ),
       body: Center(
         child: Column(
-          children: [dateBar()],
+          children: [
+            dateBar(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CircleIconButton(
+                    iconData: Icons.person,
+                    size: 50,
+                    color: Colors.white,
+                    iconColor: Color.fromARGB(255, 255, 175, 204),
+                    onPressed: () {
+                      //  TODO: Add login page here
+                      print('Button pressed');
+                    }),
+                Row(
+                  children:[
+                    Text('Board_Name '),
+                    CircleIconButton(
+                        iconData: Icons.bookmark,
+                        size: 50,
+                        color: Colors.white,
+                        iconColor: Color.fromARGB(255, 255, 175, 204),
+                        onPressed: () {
+                          // TODO: Add board page here
+                          print('Button pressed');
+                        })
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
