bc8f070d455108db617ba1412fcf8c4e3f93fd32
pooh <purichseenuallae@gmail.com>
2023-03-15 01:51:31 +0700

    Add style library

---
+++ lib/style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final primaryColor = const Color.fromARGB(255, 162, 210, 255);
+final secondaryColor = const Color.fromARGB(255, 255, 175, 204);
+final successColor = const Color.fromARGB(255, 202, 255, 191);
