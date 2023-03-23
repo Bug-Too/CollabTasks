f920afb6e284f9c782e3784e03a5f0543b765af5
pooh <purichseenuallae@gmail.com>
2023-03-24 01:08:40 +0700

    Add boardcard

--- lib/all_board_page.dart
+++ lib/all_board_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'board_card.dart';
+import 'style.dart';
+
 class AllBoardPage extends StatelessWidget {
+  Widget squareButton(Icon icon, Color color, VoidCallback onPressed) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        width: 50,
+        height: 50,
+        color: color,
+        child: Center(
+          child: Icon(icon as IconData?, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('All Board'),
-      ),
-      body: Center(
-        child: Text('All Board'),
-      ),
-    );
-  } 
+        appBar: AppBar(
+          title: Text('All Board'),
+        ),
+        body: Column(
+          children: [
+            BoardCard(
+                boardName: "boardName",
+                imageURLlist: [demoUserImage1, demoUserImage1]),
+            BoardCard(
+                boardName: "board_Name",
+                imageURLlist: [demoUserImage1, demoUserImage1])
+          ],
+        ));
+  }
 }
---
+++ lib/board_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'style.dart';
+
+class BoardCard extends StatefulWidget {
+  final String boardName;
+  final List<String> imageURLlist;
+
+  const BoardCard(
+      {super.key, required this.boardName, required this.imageURLlist});
+  @override
+  State<StatefulWidget> createState() => _BoardCardState();
+}
+
+class _BoardCardState extends State<BoardCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+              color: Colors.white,
+              height: 200,
+              alignment: Alignment.topLeft,
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                        child: Text('Board Name'),
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            color: Colors.red,
+                            child: const Center(
+                              child: Icon(Icons.delete, color: Colors.white),
+                            ),
+                          ),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            color: Colors.blue,
+                            child: const Center(
+                              child: Icon(Icons.edit, color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ))),
+    );
+  }
+}
