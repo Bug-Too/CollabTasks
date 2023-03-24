828ba7f34b149790b97313a60da7bc97e56ca60f
pooh <purichseenuallae@gmail.com>
2023-03-24 14:46:00 +0700

    Add time toggle

--- lib/widget_dialog.dart
+++ lib/widget_dialog.dart
@@ -2,108 +2,156 @@ import 'package:flutter/material.dart';
 import 'style.dart';
 import 'profiles_box.dart';
 
-class WidgetDialog extends StatefulWidget{
+class WidgetDialog extends StatefulWidget {
   final String title;
   final List<String> imageURLlist;
 
-  const WidgetDialog({super.key, required this.title, this.imageURLlist = const []});
+  const WidgetDialog(
+      {super.key, required this.title, this.imageURLlist = const []});
 
   @override
   State<StatefulWidget> createState() => _WidgetDialogState();
-
 }
 
-class _WidgetDialogState extends State<WidgetDialog>{
-    TimeOfDay selectedStartTime = TimeOfDay.now();
+class _WidgetDialogState extends State<WidgetDialog> {
+  TimeOfDay selectedStartTime = TimeOfDay.now();
   TimeOfDay selectedEndTime = TimeOfDay.now();
+  bool isAddTime = false;
   @override
   Widget build(BuildContext context) {
     return Dialog(
-        backgroundColor: primaryColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(height: 20),
-            Text("Add New Task", style: TextStyle(fontSize: 20),),
-Container(margin: EdgeInsets.fromLTRB(20, 10, 20, 0), child: profileBox(widget.imageURLlist),)
-            ,
-            
-            Container(
-              margin: EdgeInsets.fromLTRB(20, 5, 20, 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: TextField(
-                autofocus:true,
-                decoration: InputDecoration(
-                  hintText: 'Task Name',
-                  contentPadding: EdgeInsets.symmetric(horizontal: 16),
-                  border: InputBorder.none,
-                ),
+      backgroundColor: primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            "Add New Task",
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+            child: profileBox(widget.imageURLlist),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 5, 20, 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: 'Task Name',
+                contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                border: InputBorder.none,
               ),
             ),
-            Padding(padding: EdgeInsets.fromLTRB(26, 0, 26, 0),child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  child: Text("Start: ${selectedStartTime.format(context)}", style: TextStyle(color: Colors.blueAccent, backgroundColor: Colors.white),),
-                  onTap: () async {
-                    final TimeOfDay? pickedTime = await showTimePicker(
-                      context: context,
-                      initialTime: selectedStartTime,
-                    );
-                    if (pickedTime != null && pickedTime != selectedStartTime) {
-                      setState(() {
-                        selectedStartTime = pickedTime;
-                      });
-                    }
-                  },
+          ),
+          !isAddTime
+              ? Container(
+                  padding: EdgeInsets.fromLTRB(26, 0, 0, 0),
+                  child: Row(
+                    children: [
+                      InkWell(
+                          onTap: () => setState(() {
+                                isAddTime = true;
+                              }),
+                          child: Text(
+                            "+ Add Time",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                backgroundColor: Colors.white),
+                          ))
+                    ],
+                  ),
+                )
+              : Padding(
+                  padding: EdgeInsets.fromLTRB(26, 0, 26, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        child: Text(
+                          "Start: ${selectedStartTime.format(context)}",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              backgroundColor: Colors.white),
+                        ),
+                        onTap: () async {
+                          final TimeOfDay? pickedTime = await showTimePicker(
+                            context: context,
+                            initialTime: selectedStartTime,
+                          );
+                          if (pickedTime != null &&
+                              pickedTime != selectedStartTime) {
+                            setState(() {
+                              selectedStartTime = pickedTime;
+                            });
+                          }
+                        },
+                      ),
+                      Icon(Icons.forward),
+                      InkWell(
+                        child: Text(
+                          "End: ${selectedEndTime.format(context)}",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              backgroundColor: Colors.white),
+                        ),
+                        onTap: () async {
+                          final TimeOfDay? pickedTime = await showTimePicker(
+                            context: context,
+                            initialTime: selectedEndTime,
+                          );
+                          if (pickedTime != null &&
+                              pickedTime != selectedEndTime) {
+                            setState(() {
+                              selectedEndTime = pickedTime;
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-                Icon(Icons.forward),
-                InkWell(
-                  child: Text("End: ${selectedEndTime.format(context)}", style: TextStyle(color: Colors.blueAccent, backgroundColor: Colors.white),),
-                  onTap: () async {
-                    final TimeOfDay? pickedTime = await showTimePicker(
-                      context: context,
-                      initialTime: selectedEndTime,
-                    );
-                    if (pickedTime != null && pickedTime != selectedEndTime) {
-                      setState(() {
-                        selectedEndTime = pickedTime;
-                      });
-                    }
-                  },
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: secondaryColor),
+                  child: Text('Cancel'),
                 ),
-              ],
-            ),),
-            
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [InkWell(child:Container(padding: EdgeInsets.fromLTRB(20, 10, 20, 10), decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)), color: secondaryColor), 
-              
-              child: Text('Cancel'),
-              
-            ),onTap: () {
-                Navigator.of(context).pop();
-              },), InkWell(child:Container(padding: EdgeInsets.fromLTRB(20, 10, 20, 10), decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)), color: successColor), 
-              
-              child: Text('Add'),
-              
-            ),onTap: () {
-                Navigator.of(context).pop();
-              },)],)
-            , 
-            
-            
-            SizedBox(height: 20),
-          ],
-        ),
-      );
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              InkWell(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: successColor),
+                  child: Text('Add'),
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
   }
-
 }
