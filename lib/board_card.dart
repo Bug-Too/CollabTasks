6836b4abc02aac0afa681e659170265179455f98
pooh <purichseenuallae@gmail.com>
2023-03-24 16:14:44 +0700

    Add board card edit name

--- lib/board_card.dart
+++ lib/board_card.dart
@@ -1,3 +1,4 @@
+import 'package:collabtask/profiles_box.dart';
 import 'package:flutter/material.dart';
 
 import 'style.dart';
@@ -5,15 +6,22 @@ import 'style.dart';
 class BoardCard extends StatefulWidget {
   final String boardName;
   final List<String> imageURLlist;
+  final List<String> emailURLlist;
 
   const BoardCard(
-      {super.key, required this.boardName, required this.imageURLlist});
+      {super.key, required this.boardName, required this.imageURLlist, required this.emailURLlist});
   @override
   State<StatefulWidget> createState() => _BoardCardState();
 }
 
 class _BoardCardState extends State<BoardCard> {
-  bool isEdit = false;
+  bool isEdit = true;
+  TextEditingController _controller = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = widget.boardName;
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,54 +29,85 @@ class _BoardCardState extends State<BoardCard> {
       child: ClipRRect(
           borderRadius: BorderRadius.circular(10),
           child: Container(
-              color: Colors.white,
-              height: isEdit ? 200 : 50,
-              alignment: Alignment.topLeft,
-              width: double.infinity,
-              child:  Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                        child: Text('Board Name'),
-                      ),
-                      Row(
-                        children: [
-                          InkWell(
-                            onTap: () => setState(() {
-                              isEdit = !isEdit;
-                            }),
-                            child: Container(
-                              width: 50,
-                              height: 50,
-                              color: Colors.blue,
-                              child: const Center(
-                                child: Icon(Icons.edit, color: Colors.white),
+            color: Colors.white,
+            // height: isEdit ? 200 : 50,
+            alignment: Alignment.topLeft,
+            width: double.infinity,
+            child:
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                          child: Text('Board Name'),
+                        ),
+                        Row(
+                          children: [
+                            InkWell(
+                              onTap: () => setState(() {
+                                isEdit = !isEdit;
+                              }),
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: Colors.blue,
+                                child: const Center(
+                                  child: Icon(Icons.edit, color: Colors.white),
+                                ),
                               ),
                             ),
-                          ),
-                          InkWell(
-                            onTap: () => setState(() {
-                              isEdit = !isEdit;
-                            }),
-                            child: Container(
-                              width: 50,
-                              height: 50,
-                              color: Colors.red,
-                              child: const Center(
-                                child: Icon(Icons.delete, color: Colors.white),
+                            InkWell(
+                              onTap: () => setState(() {
+                                isEdit = !isEdit;
+                              }),
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: Colors.red,
+                                child: const Center(
+                                  child:
+                                      Icon(Icons.delete, color: Colors.white),
+                                ),
                               ),
                             ),
-                          ),
-                          
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              ))),
+                          ],
+                        )
+                      ],
+                    ),
+                    isEdit
+                    ? Container(
+                        margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child:
+                        Column(
+                          children: [
+                            profileBox(widget.imageURLlist),
+                            TextField(
+                              controller:  _controller,
+                              decoration: InputDecoration(
+                                hintText: 'Board Name',
+                                label: Text('Board Name'),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Row(children:[Text("Share With" ,textAlign: TextAlign.left,),]),
+                            userImage('https://picsum.photos/250?image=9', 60),
+                            
+                            
+                          ],
+                         
+                      ))
+                    : Container(),
+                    
+
+                  ],
+                  
+                ),
+                
+              
+            
+          )),
     );
   }
 }
