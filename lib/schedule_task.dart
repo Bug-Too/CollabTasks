b445a780fa069c3a301869e11d3cf621dc36c422
pooh <purichseenuallae@gmail.com>
2023-03-25 22:22:32 +0700

    add callback to schedule task

--- lib/schedule_task.dart
+++ lib/schedule_task.dart
@@ -7,8 +7,16 @@ class ScheduleTask extends StatefulWidget {
   final taskName;
   final List<String> imageURLlist;
   final timeInterval;
+  final callback;
 
-  const ScheduleTask({super.key, required this.isDone, required this.taskName, required this.imageURLlist, required this.timeInterval});
+  const ScheduleTask(
+      {super.key,
+      required this.isDone,
+      required this.taskName,
+      required this.imageURLlist,
+      required this.timeInterval,
+      required this.callback,
+      });
 
   @override
   State<StatefulWidget> createState() => _ScheduleTaskState();
@@ -23,7 +31,6 @@ class _ScheduleTaskState extends State<ScheduleTask> {
     _value = widget.isDone;
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -36,45 +43,41 @@ class _ScheduleTaskState extends State<ScheduleTask> {
         },
         onLongPress: () {
           //TODO: Show popup here
-          setState(() {
-            _value = !_value;
-          });
-          widget.timeInterval[0].toString();
+          widget.callback(widget.taskName, widget.timeInterval[0], widget.timeInterval[1]);
         },
         child: Container(
-            decoration: BoxDecoration(
-              color: _value ? successColor : secondaryColor,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            
-                padding: EdgeInsets.all(26),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    
-                    Flexible(child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        
-                        _value
-                            ? Text(
-                                widget.taskName,
-                                style: TextStyle(
-                                    decoration: TextDecoration.lineThrough),
-                              )
-                            : Text(widget.taskName,style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16
-                                    )),Text('Time: 10:00AM - 12:00PM'),
-                        //  profileBox(widget.imageURLlist),
-                      ],
-                    ))
-                    ,Icon(
-                          _value ? Icons.done : Icons.circle,
-                          color: _value ? Colors.black : Colors.white,
+          decoration: BoxDecoration(
+            color: _value ? successColor : secondaryColor,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          padding: EdgeInsets.all(26),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _value
+                      ? Text(
+                          widget.taskName,
+                          style:
+                              TextStyle(decoration: TextDecoration.lineThrough),
                         )
-                  ],
-                ),),
+                      : Text(widget.taskName,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text('Time: 10:00AM - 12:00PM'),
+                  //  profileBox(widget.imageURLlist),
+                ],
+              )),
+              Icon(
+                _value ? Icons.done : Icons.circle,
+                color: _value ? Colors.black : Colors.white,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
